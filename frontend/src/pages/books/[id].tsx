d2db01cba425090099576b83ca87b260cfc427f4
tezkerek <andrei.ancuta@gmail.com>
2023-04-12 22:28:14 +0300

    Fix title element issue

--- frontend/src/pages/books/[id].tsx
+++ frontend/src/pages/books/[id].tsx
@@ -15,7 +15,7 @@ export default function BookDetailPage() {
     return (
         <>
             <Head>
-                <title>{mockBook.title} | MinnieBooks</title>
+                <title>{`${mockBook.title} | MinnieBooks`}</title>
                 <meta name="description" content={id} />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
