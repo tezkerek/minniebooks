99579298d2777ce05c3c83a7c8eb7b5e179966bf
stefan <diaconustefan16@gmail.com>
2023-04-17 14:26:13 +0300

    remove {""}

--- frontend/src/pages/search.tsx
+++ frontend/src/pages/search.tsx
@@ -99,7 +99,7 @@ export default function SearchPage() {
   return (
     <>
       <Head>
-        <title>{"Search | MinnieBooks"}</title>
+        <title>Search | MinnieBooks</title>
         <meta name="description" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
