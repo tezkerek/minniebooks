b824bcf749168a2b80a1da76294adb4c8a185b0f
andrei.lefter <92049917+andlft@users.noreply.github.com>
2023-04-24 13:37:02 +0300

    Add register

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -4,7 +4,7 @@ from books.serializers import ReviewSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
-    profile_picture = serializers.FileField()
+    profile_picture = serializers.FileField(required=False)
 
     class Meta:
         model = MinnieBooksUser
@@ -12,3 +12,28 @@ class UserSerializer(serializers.ModelSerializer):
                   'date_joined', 'profile_picture', 'reviews',
                   'is_admin', 'is_employee', 'is_active', 'recommendations', 'quotes', 'progress_updates', 'likes']
         read_only_fields = ['id']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    profile_picture = serializers.FileField(required=False)
+    password = serializers.CharField(
+        min_length= 8,
+        write_only=True,
+        required = True,
+        style={'input_type': 'password', 'placeholder': ''}
+    )
+
+    class Meta:
+        model = MinnieBooksUser
+        fields = ['first_name', 'last_name', 'email', 'profile_picture', 'password']
+
+    def create(self, validated_data):
+        user = MinnieBooksUser.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        return user
+
+
+
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,19 @@
 from rest_framework import mixins, viewsets
+from rest_framework.permissions import AllowAny
 
 from .models import MinnieBooksUser
-from .serializers import UserSerializer
+from .serializers import UserSerializer, RegisterSerializer
+from books.permissions import IsAdminOrReadOnly
+
+class RegisterViewSet(mixins.CreateModelMixin,
+                      viewsets.GenericViewSet):
+    
+    queryset = MinnieBooksUser.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+
+
 
 
 class UserViewSet(mixins.ListModelMixin,
@@ -10,6 +22,8 @@ class UserViewSet(mixins.ListModelMixin,
                   mixins.UpdateModelMixin,
                   mixins.DestroyModelMixin,
                   viewsets.GenericViewSet):
+    
     queryset = MinnieBooksUser.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
