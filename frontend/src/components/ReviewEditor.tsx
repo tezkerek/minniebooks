ac4ea56dc770f8fa3647e9c241f500326ebe7989
stefan <diaconustefan16@gmail.com>
2023-05-03 10:47:07 +0300

    move component to new file

---
+++ frontend/src/components/ReviewEditor.tsx
@@ -0,0 +1,67 @@
+import { css } from "@emotion/react";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
+import Button from "@mui/material/Button";
+import { useState } from "react";
+import RatingSelector from "@/components/RatingSelector";
+
+interface Review {
+  text: string;
+  rating: number;
+}
+
+interface ReviewEditorProps {
+  onDone: (review: Review) => void;
+}
+
+export default function ReviewEditor({
+  onDone,
+}: ReviewEditorProps): JSX.Element {
+  const [reviewText, setReviewText] = useState<string>("");
+  const [rating, setRating] = useState<number>(0);
+  const finalReview: Review = { text: reviewText, rating };
+
+  return (
+    <>
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          align-items: center;
+        `}
+      >
+        <p>Write review</p>
+
+        <RatingSelector
+          maxRating={5}
+          rating={rating}
+          onRatingChange={setRating}
+        />
+
+        <TextareaAutosize
+          minRows={10}
+          css={css`
+            display: block;
+            width: 30vh;
+            font-family: Arial, Helvetica, sans-serif;
+          `}
+          value={reviewText}
+          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+            setReviewText(e.target.value);
+          }}
+        />
+        <Button
+          css={css`
+            margin: 8px;
+          `}
+          color="warning"
+          variant="outlined"
+          disabled={!(rating >= 1 && rating <= 5)}
+          onClick={() => onDone(finalReview)}
+        >
+          Post review
+        </Button>
+      </div>
+    </>
+  );
+}
--- frontend/src/pages/books/[id].tsx
+++ frontend/src/pages/books/[id].tsx
@@ -4,55 +4,6 @@ import { css } from "@emotion/react"
 import BookDetail from "@/components/BookDetail"
 import Navbar from "@/components/Navbar"
 import ReviewList from "@/components/ReviewList"
-import TextareaAutosize from "@mui/material/TextareaAutosize";
-import Button from "@mui/material/Button";
-import { useState } from "react";
-import RatingSelector from "@/components/RatingSelector"
-
-
-function ReviewEditor(): JSX.Element {
-  const [reviewText, setReviewText] = useState<string>("");
-  const [rating, setRating] = useState<number>(0);
-
-  return (
-    <>
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          justify-content: space-between;
-          align-items: center;
-        `}
-      >
-        <p>Write review</p>
-
-        <RatingSelector maxRating={5} rating={rating} onRatingChange={setRating} />
-
-        <TextareaAutosize
-          css={css`
-            display: block;
-          `}
-          value={reviewText}
-          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
-            setReviewText(e.target.value);
-          }}
-        />
-        <Button
-        
-          color="warning"
-          variant="outlined"
-          disabled={!(rating >= 1 && rating <= 5)}
-          onClick={() => {
-            console.log(reviewText, rating);
-          }}
-        >
-          Post review
-        </Button>
-      </div>
-    </>
-  );
-}
-
 
 const mockBook = { id: 1, title: "Book1", author: "Author1", rating: 3, coverImageUrl: 'https://picsum.photos/300/480' }
 const reviews = [{ id: 1, rating: 4, text: "Pretty good book", authorUsername: 'the_critic' }, { id: 2, rating: 2, text: "GarBAGE", authorUsername: 'hater' }]
