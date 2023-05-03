95a320a86029a442a256744af0c8625dc5af6f79
tezkerek <andrei.ancuta@gmail.com>
2023-05-03 14:43:13 +0300

    Rethrow HTTP error in fetcher

--- frontend/src/api/fetcher.ts
+++ frontend/src/api/fetcher.ts
@@ -1,3 +1,10 @@
-const fetcher = (url: string) => fetch(`http://127.0.0.1:8000/${url}`).then(response => response.json())
+const fetcher = (url: string) =>
+  fetch(`http://127.0.0.1:8000/${url}`).then((response) =>
+    response.ok
+      ? response.json()
+      : response.json().then((json) => {
+          throw new Error(json);
+        })
+  )
 
-export default fetcher;
+export default fetcher
