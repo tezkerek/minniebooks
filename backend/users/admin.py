10b9c9b00312f7f70a41b2205b2a347f8400d03e
andrei.lefter <92049917+andlft@users.noreply.github.com>
2023-05-05 11:23:09 +0300

    Register FriendRequest model in admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -8,9 +8,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.core.exceptions import ValidationError
 
-from .models import MinnieBooksUser
-
-# Register your models here.
+from .models import MinnieBooksUser, FriendRequest
 
 
 class UserCreationForm(forms.ModelForm):
@@ -47,13 +45,8 @@ class UserChangeFrom(forms.ModelForm):
 
 
 class UserAdmin(BaseUserAdmin):
-    # The forms to add and change user instances
     form = UserChangeFrom
     add_form = UserCreationForm
-
-    # The fields to be used in displaying the User model.
-    # These override the definitions on the base UserAdmin
-    # that reference specific fields on auth.User.
 
     list_display = ("email", "is_admin")
     list_filter = ("is_admin",)
@@ -93,8 +86,7 @@ class UserAdmin(BaseUserAdmin):
     filter_horizontal = ()
 
 
-# Now register the new UserAdmin...
 admin.site.register(MinnieBooksUser, UserAdmin)
-# ... and, since we're not using Django's built-in permissions,
-# unregister the Group model from admin.
+admin.site.register(FriendRequest)
+
 admin.site.unregister(Group)
