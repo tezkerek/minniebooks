a295a6e7202011e7bb86854fe011f1b0d2149144
tezkerek <andrei.ancuta@gmail.com>
2023-05-06 03:58:44 +0300

    Fix dark mode CSS

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
+import React from "react";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme, useMediaQuery } from "@mui/material";
+import type { AppProps } from "next/app";
+import "@/styles/globals.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
