ef363f3250a077ee5edfee321f5fbc2ce17a5d45
tezkerek <andrei.ancuta@gmail.com>
2023-05-06 17:57:38 +0300

    Use SessionAuthentication as well

--- backend/minniebooks/settings.py
+++ backend/minniebooks/settings.py
@@ -114,6 +114,7 @@ AUTH_USER_MODEL = "users.MinnieBooksUser"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ]
 }
 
