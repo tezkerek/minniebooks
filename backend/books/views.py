8900696715b49dae0e735eec4899558a2adee158
andrei.lefter <92049917+andlft@users.noreply.github.com>
2023-05-07 16:41:30 +0300

    Remove unnecessary import

--- backend/books/views.py
+++ backend/books/views.py
@@ -3,7 +3,6 @@ from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import filters
-from rest_framework.decorators import action
 from django.db.models import Avg
 from .models import (
     Author,
