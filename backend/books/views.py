49c9a589be7eff8ed93acd7ec0db7e86287574cb
tezkerek <andrei.ancuta@gmail.com>
2023-06-01 14:44:28 +0300

    Remove unnecessary import

--- backend/books/views.py
+++ backend/books/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from rest_framework.views import APIView
-from rest_framework import filters, serializers
+from rest_framework import filters
 from django.db.models import Avg
 from .models import (
     Author,
